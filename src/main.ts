bbde642ccc68e344dcc5cb4282260ab3355e7660
Anna Myronova <annamyronova8@gmail.com>
2024-12-16 22:50:04 +0200

    Fixed multi-line output

--- src/main.ts
+++ src/main.ts
@@ -31,29 +31,62 @@ const secondSongLine: string[] = [
   "location ",
 ];
 
+const thirdSongLine: string[] = [
+  `It's `,
+  "understood ",
+  "that ",
+  "Hollywood ",
+  "sells ",
+  "Californication ",
+];
+
 const firstLineTimeout: number[] = [
-  100, 150, 450, 600, 750, 1000, 1500, 2000, 2250, 2500, 2750, 3500,
+  200, 200, 200, 200, 200, 200, 200, 200, 225, 250, 200, 200,
 ];
 
 const secondLineTimeout: number[] = [
-  3800, 4000, 4300, 4700, 5350, 5750, 5950, 6150, 6450, 6800, 7050, 7350, 7700,
-  8050, 8300,
+  200, 200, 200, 200, 200, 200, 200, 200, 225, 250, 200, 200, 200, 200, 200,
 ];
 
-const songLyrics: string[][] = [firstSongLine, secondSongLine];
-const songTimeouts: number[][] = [firstLineTimeout, secondLineTimeout];
+const thirdLineTimeout: number[] = [500, 800, 400, 1000, 800, 1200];
+
+const songLyrics: string[][] = [firstSongLine, secondSongLine, thirdSongLine];
+const songTimeouts: number[][] = [
+  firstLineTimeout,
+  secondLineTimeout,
+  thirdLineTimeout,
+];
 
 let songOutput: string = "";
+let alreadyShownLines: string = '';
 
-for (let line = 0; line < songLyrics.length; line++) {
-  for (let songWordIndex = 0; songWordIndex < songLyrics[line].length; songWordIndex++) {
-    setTimeout(() => {
-      console.clear();
-      songOutput += songLyrics[line][songWordIndex];
-      console.log(songOutput);
-      if(songWordIndex === songLyrics[line].length - 1) {
-        songOutput = '';
-      }
-    }, songTimeouts[line][songWordIndex]);
+async function playSong() {
+  for (let line = 0; line < songLyrics.length; line++) {
+    for (
+      let songWordIndex = 0;
+      songWordIndex < songLyrics[line].length;
+      songWordIndex++
+    ) {
+      await new Promise<void>((resolve) =>
+        setTimeout(() => {
+          console.clear();
+          const cuttedOddElements: string = alreadyShownLines.slice(0, -2);
+          console.log(cuttedOddElements);
+          songOutput += songLyrics[line][songWordIndex];
+          if (songWordIndex === songLyrics[line].length - 1) {
+            console.log(songOutput);
+            alreadyShownLines += songOutput + '\n';
+            songOutput = "";
+          } else {
+            console.log(songOutput);
+          }
+          resolve();
+        }, songTimeouts[line][songWordIndex])
+      );
+    }
   }
+          console.clear();
+          console.log(alreadyShownLines);
 }
+
+playSong();
